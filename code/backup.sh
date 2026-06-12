6b4737d8f78bdec73ceaaa4cebe3c69f3515aad8
0x9z <270520765+0x9z@users.noreply.github.com>
2026-06-12 18:03:19 -0400

    Update backup.sh

--- code/backup.sh
+++ code/backup.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+#a bash script to make easy backups
+
+#you can change the folder/path source
+SOURCE="$HOME/Documents"
+
+#you can change where to save BACKUPS olders
+BACKUP_DIR="$HOME/shell/github/linux/backup-tools/BACKUPS"
+
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+
+FILENAME="backup_$DATE.tar.gz"
+
+mkdir -p "$BACKUP_DIR"
+
+tar -czf "$BACKUP_DIR/$FILENAME" "$SOURCE"
+
+echo "BACKUP CREATED SECCESSFULLY: $BACKUP_DIR/$FILENAME"
